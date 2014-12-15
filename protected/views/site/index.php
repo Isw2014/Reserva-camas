be079f598067113d30b97e49110c586bf1be0fc9
FIMR <Kruger_1000@hotmail.com>
2014-12-14 22:29:50 -0400

    descartar imagenes

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,6 +5,4 @@ $this->pageTitle=Yii::app()->name;
 ?>
 <div class="background site" id="div1">
 <img src="http://i72.photobucket.com/albums/i182/Kruger182/background_zpsa1d97d8e.png" border="0" alt=" photo background_zpsa1d97d8e.png"/></a>
-<a href="http://127.0.0.1/yii2/Reserva-camas/index.php/paciente/admin"><img src="http://i72.photobucket.com/albums/i182/Kruger182/logopacientes_zps6e4ff1cc.png" border="0" alt=" photo logopacientes_zps6e4ff1cc.png"/></a>
-<a href="http://127.0.0.1/yii2/Reserva-camas/index.php/cama/admin"><img src="http://i72.photobucket.com/albums/i182/Kruger182/bed-md_zps883201a3.png" border="0" alt=" photo bed-md_zps883201a3.png"/></a>
 </div>
